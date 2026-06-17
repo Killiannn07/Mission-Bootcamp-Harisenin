de96c701120fe30ff263a7f53333c98eff9d516f
Killiannn07 <sofyanyunianto.work@gmail.com>
2026-06-17 11:05:46 +0700

    feat: login page & register page

--- Mission 3/script.js
+++ Mission 3/script.js
@@ -1,5 +1,13 @@
-const title = document.getElementById('title');
-
-title.addEventListener('click', function() {
-    alert('You clicked the title!');
-});
+function togglePassword(button) {
+  const passwordWrapper = button.parentElement;
+  const passwordInput = passwordWrapper.querySelector('.password-input');
+  const img = button.querySelector('img');
+  
+  if (passwordInput.type === 'password') {
+    passwordInput.type = 'text';
+    img.src = './assets/eye-off.svg';
+  } else {
+    passwordInput.type = 'password';
+    img.src = './assets/eye-off.svg';
+  }
+}
