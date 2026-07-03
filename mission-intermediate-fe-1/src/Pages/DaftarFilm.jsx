5ed35ecee5e6d37827dd8d7aadd97b6a7dca4289
Killiannn07 <sofyanyunianto.work@gmail.com>
2026-07-03 22:23:45 +0700

    feat: list film page

---
+++ mission-intermediate-fe-1/src/Pages/DaftarFilm.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { MainLayout } from "../Components/layout/MainLayout";
+import { movies } from "../data/movie";
+import { MovieCardPotrait } from "../Components/movie/MovieCard";
+
+export const DaftarFilm = () => {
+  return (
+    <MainLayout>
+      <div className="py-1.5 px-5 md:py-6 md:px-20">
+        <h1 className="text-xl font-bold text-left mb-4">Daftar Saya</h1>
+        <div className="grid grid-cols-3 gap-4 md:grid-cols-4 lg:grid-cols-6">
+          {movies.map((movie) => (
+            <MovieCardPotrait key={movie.id} {...movie} />
+          ))}
+        </div>
+      </div>
+    </MainLayout>
+  );
+};
--- mission-intermediate-fe-1/src/Routes/AppRoutes.jsx
+++ mission-intermediate-fe-1/src/Routes/AppRoutes.jsx
@@ -4,6 +4,7 @@ import { LoginPage } from "../Pages/Login";
 import { RegisterPage } from "../Pages/Register";
 import { HomePage } from "../Pages/HomePage";
 import { ProfilPage } from "../Pages/Profil";
+import { DaftarFilm } from "../Pages/DaftarFilm";
 
 export const AppRoutes = () => {
   return (
@@ -12,6 +13,7 @@ export const AppRoutes = () => {
       <Route path="/register" element={<RegisterPage/>} />
       <Route path="/" element={<HomePage/>} />
       <Route path="/profil" element={<ProfilPage/>} />
+      <Route path="/daftarfilm" element={<DaftarFilm/>} />
 
     </Routes>
   );
