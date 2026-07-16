9c5acc22cd1e4b4dfd0cce3c9368f59b8d6efb40
Killiannn07 <sofyanyunianto.work@gmail.com>
2026-07-16 21:31:00 +0700

    setup structure backend & frontend

---
+++ MovieApp/Backend/src/config/db.js
@@ -0,0 +1,19 @@
+import pkg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const { Pool } = pkg;
+
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+});
+
+const result = await pool.query("SELECT NOW()");
+console.log(result.rows);
+
+export default pool;
